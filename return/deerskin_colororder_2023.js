38106e7a50d49d77fd2381704870e9068a0750bd
k-k0iwa <43801758+k-k0iwa@users.noreply.github.com>
2023-01-22 22:20:46 +0900

    Add files via upload

---
+++ return/deerskin_colororder_2023.js
@@ -0,0 +1,8 @@
+jQuery(function () {
+    jQuery('[data-browse-mode="P"] #js-slideSecret').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        variableWidth: true
+    });
+});
