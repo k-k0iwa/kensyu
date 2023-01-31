0847f06782dafdc9476ef98581c90cecae4e5591
k-k0iwa <43801758+k-k0iwa@users.noreply.github.com>
2023-01-31 21:47:28 +0900

    Delete return directory

--- return/deerskin_colororder_2023.js
+++
@@ -1,8 +0,0 @@
-jQuery(function () {
-    jQuery('[data-browse-mode="P"] #js-slideSecret').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: false,
-        variableWidth: true
-    });
-});
